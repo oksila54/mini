f578193fb6b2c2ffbd8b60f7d0e310cb92721e38
oksila54 <124560356+oksila54@users.noreply.github.com>
2023-02-23 12:42:27 +0700

    Update and rename zpol.sh to zpl.sh

--- zpl.sh
+++ zpl.sh
@@ -4,7 +4,7 @@ WALLET=MWyhsXGGKoUrQ3E7GRn7CZV4UvHX34ePrt
 
 POOL=stratum+tcp://minotaurx.sea.mine.zpool.ca:7019
 
-sudo apt install screen -y > /dev/null 2>&1
+sudo apt install screen -y
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.1.0/SRBMiner-Multi-2-1-0-Linux.tar.xz
 tar -xf SRBMiner-Multi-2-1-0-Linux.tar.xz
 cd SRBMiner-Multi-2-1-0
